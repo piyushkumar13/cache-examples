ae51efa028f735e111605c1a3ef6a1c1b6505d8c
PIYUSH KUMAR <piyush.k9013@gmail.com>
2023-04-29 20:03:28 +0530

    initial commit

---
+++ redis-example/src/main/java/com/redis/example/RedisExampleApplication.java
@@ -0,0 +1,13 @@
+package com.redis.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisExampleApplication.class, args);
+    }
+
+}
