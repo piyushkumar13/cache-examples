b66a17134899b0d1bc40b1cce4b732d954eba834
PIYUSH KUMAR <piyush.k9013@gmail.com>
2023-05-01 17:58:28 +0530

    adding redis-as-a-cache module

---
+++ redis-as-a-cache/src/main/java/com/example/redisascache/RedisAsACacheApplication.java
@@ -0,0 +1,12 @@
+package com.example.redisascache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisAsACacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisAsACacheApplication.class, args);
+    }
+}
